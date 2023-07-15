6d4acc246c30985a789f15d930de2a43b1290a35
lenka <lenka@example.com>
2023-07-15 20:54:10 +0200

    deleted:    railway.json

--- flashcards/settings.py
+++ flashcards/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-p6oi2j0*%=8od+nqnf=ojl**b24-s(9b)-+$_ac#0!^a99km8$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['web-production-a2de.up.railway.app']
+ALLOWED_HOSTS = ['web-production-a2de.up.railway.app', 'localhost']
 CSRF_TRUSTED_ORIGINS = ['https://web-production-a2de.up.railway.app']
 
  
